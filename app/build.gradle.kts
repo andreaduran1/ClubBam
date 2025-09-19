03e9e0410285bc6dce90a5f4cc89eab9225fb6ac
Marce <roigmar.bagmail.com>
2025-09-19 17:49:08 -0300

    Creación de la pantalla de login. Creación de archivo de colores y theme. Intento fallido de aplicar Material Design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,10 +39,12 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.android.material:material:1.13.0")
 }
